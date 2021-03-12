62d9c0df5ff40f86ce276150abde12bad51b2625
alewerner <xandewerner.2@gmail.com>
2021-03-12 17:40:17 -0300

    add swagger

--- src/main/java/com/casadocodigo/basic/livraria/autor/AutorController.java
+++ src/main/java/com/casadocodigo/basic/livraria/autor/AutorController.java
@@ -1,25 +1,24 @@
 package com.casadocodigo.basic.livraria.autor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 @RestController
 public class AutorController {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    @Autowired
+    private AutorRepository autorRepository;
 
-    @PostMapping(value = "/autores")
+    @PostMapping(value = "/api/autores")
     @Transactional
     public String criar(@RequestBody NovoAutorRequest request) {
 
         Autor autor = request.toModel();
-        entityManager.persist(autor);
+        autorRepository.save(autor);
 
         return  autor.toString();
     }
