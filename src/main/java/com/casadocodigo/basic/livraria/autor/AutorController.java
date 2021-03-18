f0fbdfc3d133825eed8c3008a9250d500208797d
Josias Melo Vidal <josias_vidal@terceiros.sicredi.com.br>
2021-03-18 18:13:42 -0300

    metodo para listar todos, buscar por autor, alteracao do repository e nova classe dto

--- src/main/java/com/casadocodigo/basic/livraria/autor/AutorController.java
+++ src/main/java/com/casadocodigo/basic/livraria/autor/AutorController.java
@@ -1,8 +1,12 @@
 package com.casadocodigo.basic.livraria.autor;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.transaction.Transactional;
@@ -21,5 +25,17 @@ public class AutorController {
         autorRepository.save(autor);
 
         return  autor.toString();
+    }
+
+    @GetMapping(value= "/api/autores/{nome}")
+    public AutorDto detalhar(@RequestParam @PathVariable String nome){
+        Autor autor = autorRepository.findByNome(nome);
+        return new AutorDto(autor);
+    }
+
+    @GetMapping(value="/api/autores/listarTodos")
+    public List<AutorDto> listarTodos(){
+        List<Autor> autores = autorRepository.findAll();
+        return AutorDto.converter(autores);
     }
 }
--- src/main/java/com/casadocodigo/basic/livraria/autor/AutorRepository.java
+++ src/main/java/com/casadocodigo/basic/livraria/autor/AutorRepository.java
@@ -1,15 +1,19 @@
 package com.casadocodigo.basic.livraria.autor;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface AutorRepository extends CrudRepository<Autor, Long> {
+public interface AutorRepository extends JpaRepository<Autor, Long> {
 
     @Override
     Optional<Autor> findById(Long aLong);
 
-    Optional<Autor> findByEmail(String email);
+    List<Autor> findByEmail(String email);
+
+    Autor findByNome(String nome);
+
 }
