dbdcc1f05a93d2757851318b5811ae41f6c2aebb
Josias Melo Vidal <josias_vidal@terceiros.sicredi.com.br>
2021-04-15 17:45:28 -0300

    criação do repositorio

---
+++ src/main/java/com/casadocodigo/basic/livraria/livro/LivroRepository.java
@@ -0,0 +1,7 @@
+package com.casadocodigo.basic.livraria.livro;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+}
